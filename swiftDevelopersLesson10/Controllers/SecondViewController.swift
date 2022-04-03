62d4b748394fbe070088897987c42984559ff6e8
Andrew <Andreu_s@tut.by>
2022-04-04 00:55:12 +0300

    1

--- swiftDevelopersLesson10/Controllers/SecondViewController.swift
+++ swiftDevelopersLesson10/Controllers/SecondViewController.swift
@@ -14,13 +14,18 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         title = "Food"
+        pizzaLabelSetup()
 
     }
     
 
    // MARK: - setUp labels
     
-
+    fileprivate func pizzaLabelSetup(){
+        pizzaLabel.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        pizzaLabel.textColor = .orange
+        pizzaLabel.text = "Пицца"
+        view.addSubview(pizzaLabel)
+    }
 }
